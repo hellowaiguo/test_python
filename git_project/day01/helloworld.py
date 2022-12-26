93ccd2d3705aabbde392c18a1626ab84706fb85f
tyc <tyc@example.com>
2022-12-26 19:28:02 +0800

    提交helloworld

--- git_project/day01/helloworld.py
+++ git_project/day01/helloworld.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 """
 helloworld.py
-Created on 2022/12/26 19:21
+Created on 2022/12/26 19:26
 Copyright (c) 2022/12/26, 海牛学院版权所有.
 @author: 田亚超
 """
-print("hello world")
+print("helloworld!!!")
